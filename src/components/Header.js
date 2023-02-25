ff4198c476213f7782b35d725837472a5041e3e8
Chris Widstrom <cwidstrom@me.com>
2023-02-25 11:54:56 -0600

    adds buttons and logo to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,47 @@
 import React from "react";
 import styled from "styled-components";
+import Button from '@mui/material/Button';
+import logo from '../img/omni_logo.png';
 
 export const Header = () => {
     return (
-        <SContainer>
-            <SColumn1>
-                Menu
-                Omni
-            </SColumn1>
-            <SColumn2>
-                Become Member
-                Log in
-            </SColumn2>
-        </SContainer>
+
+        <>
+        <SHeaderTop>
+            <div>
+                <span>Menu</span>
+                <SLogo src={logo}/>
+            </div>
+            <div>
+                <Button variant="outlined" size="small">Become Member</Button>
+                <Button variant="contained" size="small" disableElevation>Log in</Button>
+            </div>
+        </SHeaderTop>
+
+        <SHeaderBottom>
+            <span>Top News</span>
+            <span>Latest News</span>
+            <span>Personalized News Feed</span> 
+        </SHeaderBottom>
+        </>
     )
 }
 
-const SContainer = styled.div`
-    background: green;
+const SHeaderTop = styled.div`
+    background: white;
     display: flex;
     justify-content: space-between;
+    align-items: center;
+
+    height: 50px;
 `
-const SColumn1 = styled.div`
 
+const SHeaderBottom = styled.div`
+    background: lightgray;
+    display: flex;
+    justify-content: space-evenly;
 `
 
-const SColumn2 = styled.div`
-
+const SLogo = styled.img`
+    width: 70px;
 `
