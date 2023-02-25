139b167293ab17937a0abde081c0b917adb0de05
Chris Widstrom <cwidstrom@me.com>
2023-02-25 15:58:50 -0600

    adds media query for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,82 @@
 import React from "react";
 import styled from "styled-components";
-import Button from '@mui/material/Button';
-import logo from '../img/omni_logo.png';
+import Button from "@mui/material/Button";
+import logo from "../img/omni_logo.png";
 
 export const Header = () => {
-    return (
+  return (
+    <SContainer>
+      <SLogoAndMenu>
+        <span>Menu</span>
+        <SLogo src={logo} />
+      </SLogoAndMenu>
 
-        <>
-        <SHeaderTop>
-            <div>
-                <span>Menu</span>
-                <SLogo src={logo}/>
-            </div>
-            <div>
-                <Button variant="outlined" size="small">Become Member</Button>
-                <Button variant="contained" size="small" disableElevation>Log in</Button>
-            </div>
-        </SHeaderTop>
+      <SButtons>
+        <Button variant="outlined" size="small">
+          Become Member
+        </Button>
+        <Button variant="contained" size="small" disableElevation>
+          Log in
+        </Button>
+      </SButtons>
 
-        <SHeaderBottom>
-            <span>Top News</span>
-            <span>Latest News</span>
-            <span>Personalized News Feed</span> 
-        </SHeaderBottom>
-        </>
-    )
-}
+      <SLinks>
+        <span>Top News</span>
+        <span>Latest News</span>
+        <span>Personalized News Feed</span>
+      </SLinks>
+    </SContainer>
+  );
+};
 
-const SHeaderTop = styled.div`
+const SContainer = styled.div`
+  display: grid;
+  grid-template-areas:
+    "logoAndMenu buttons"
+    "links links";
+    @media screen and (min-width: 1024px) {
+    grid-template-areas:
+        "logoAndMenu links buttons"
+  }
+`;
+
+const SLogoAndMenu = styled.div`
+  grid-area: logoAndMenu;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  background: white;
+  height: 50px;
+  border-bottom: 0.7px solid lightgray;
+`;
+
+const SButtons = styled.div`
+  grid-area: buttons;
+  display: flex;
+  justify-content: flex-end;
+
+  background: white;
+  border-bottom: 0.7px solid lightgray;
+`;
+
+const SLinks = styled.div`
+  grid-area: links;
+  display: flex;
+  align-items: center;
+
+  background: #FAFAFA;
+  height: 30px;
+  box-shadow: 1px 1px 3px lightgray;
+
+  @media screen and (min-width: 1024px) {
     background: white;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    height: 50px;
-`
-
-const SHeaderBottom = styled.div`
-    background: lightgray;
-    display: flex;
-    justify-content: space-evenly;
-`
+    box-shadow: none;
+    border-bottom: 0.7px solid lightgray;
+    height: 100%;
+  }
+`;
 
 const SLogo = styled.img`
-    width: 70px;
-`
+  width: 70px;
+`;
